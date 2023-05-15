9e54b7c2a4a7267c1c7e6f0496127ed21cdd6be3
nicodwirahman <nico.dwi.ft22@mail.umy.ac.id>
2023-05-15 19:04:27 +0700

    z

--- staticfungstion_156/chainfunction.cpp
+++ staticfungstion_156/chainfunction.cpp
@@ -1,3 +1,24 @@
 #include <iostream>
 using namespace std;
 class buku
+{
+	string judul;
+
+public:
+	buku setjudul(string judul)
+	{
+		this->judul = judul;
+		return *this;
+	}
+	string getjudul()
+	{
+		return this->judul;
+	}
+
+} bukunya;
+
+int main()
+{
+	cout << bukunya.setjudul("matematika").getjudul();
+	return;
+}
