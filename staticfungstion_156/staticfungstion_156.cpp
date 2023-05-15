2dd6f93b9a72bf4e3c8302dbedcd05814373959f
nicodwirahman <nico.dwi.ft22@mail.umy.ac.id>
2023-05-15 17:56:32 +0700

    2

--- staticfungstion_156/staticfungstion_156.cpp
+++ staticfungstion_156/staticfungstion_156.cpp
@@ -1,3 +1,21 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class mahasiswa {
+private:
+	static int nim;
+public:
+	int id;
+	string nama;
+
+	void setID();
+	void printAll();
+
+	static void setNIim(int pNim) { nim = pNim; }
+	static int getNim() { return nim; }
+	mahasiswa(string pnama) :nama(pnama) { setID(); }
+		
+};
+
+
