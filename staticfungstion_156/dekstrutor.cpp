49680620673ddc60efdf6d5e425973a85efcf362
nicodwirahman <nico.dwi.ft22@mail.umy.ac.id>
2023-05-15 19:22:26 +0700

    13

--- staticfungstion_156/dekstrutor.cpp
+++ staticfungstion_156/dekstrutor.cpp
@@ -11,3 +11,17 @@ public:
 	void cetakData();
 	void isiData();
 };
+
+//definisi member function
+angka::angka(int i) { //constructor
+	panjang = i;
+	arr = new int[i];
+	isiData();
+}
+
+angka::~angka() { //Destructor
+	cout << endl;
+	cetakData();
+	delete[]arr;
+	cout << "Alamat Array Sudah Dilepaskan" << endl;
+}
